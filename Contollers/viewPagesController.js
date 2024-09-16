bfaecd2081ab0efe909c7bf9f517168af3ca653a
EffaOjah <effaojah@gmail.com>
2024-09-16 17:15:58 +0100

    another

--- Contollers/viewPagesController.js
+++ Contollers/viewPagesController.js
@@ -24,21 +24,37 @@ router.post('/couponcode/info', (req, res)=>{
     const coupon = req.body.coupon;
 
     // Check if the coupon is valid
-    connection.query('SELECT * FROM users INNER JOIN registeration_tokens ON users.user_id = registeration_tokens.user_id WHERE token = ?', coupon, (err, result)=>{
+    connection.query('SELECT * FROM registeration_tokens WHERE token = ?', coupon, (err, result)=>{
         if (err) {
             console.log(err);
             
         } else{
             console.log('Coupon Info: ', result);
 
-            res.render('Verify Coupon Result', {coupon: result});
+            // Get the username of the user with inner join
+            connection.query('SELECT * FROM users INNER JOIN registeration_tokens ON users.user_id = registeration_tokens.user_id WHERE token = ?', coupon, (err, info)=>{
+                if (err) {
+                    console.log(err);
+                } else{
+                    console.log(info);
+                    res.render('Verify Coupon Result', {coupon: result, info});
+                }
+            });
         }
     });
 });
 
 // Top earners route
 router.get('/top-earners', (req, res)=>{
-    res.render('Top earners');
+    // Get the top earners
+    connection.query('SELECT u.username, t.user_id, SUM(t.amount) AS balance FROM affiliate_transactions t JOIN users u ON t.user_id = u.user_id GROUP BY t.user_id, u.username ORDER BY balance DESC', (err, result)=>{
+        if (err) {
+            console.log(err);
+        } else{
+            console.log('Result: ', result);
+            res.render('Top Earners', {earners: result});
+        }
+    });
 });
 
 // Terms route
